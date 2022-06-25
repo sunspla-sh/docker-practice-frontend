1d659d68e0c78e5b560cb5896779eefd4147244a
jack <jrfisch95@gmail.com>
2022-06-25 12:56:12 -0400

    add docker-compose.yaml for running test service simultaneously with web app, add more tests to verify that jest is functioning correctly, add readme.md

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Info
+
+---
+
+## Commands
+
+ - ```docker run -p 5000:3000 -v $(pwd):/usr/app sunsplash/docker-demo-frontend``` for HMR in running docker container
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,3 +1,18 @@
-test('test that jest is working', () => {
-  expect(true).toBe(true)
+describe('example suite', () => {
+  test('test that jest is working', () => {
+    expect(true).toBe(true)
+  })
+  
+  test('test that jest hot reloading is working', () => {
+    expect(true).toBe(true)
+  })  
+})
+
+describe('second example suite', () => {
+  test('test that second suite is working', () => {
+    expect(true).toBe(true)
+  })
+  test('test that docker-compose test service is working', () => {
+    expect(true).toBe(true)
+  })
 })
